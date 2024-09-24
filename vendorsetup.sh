5fa45366bdc06b14f065536b1c2902a480e0bf34
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-09-24 22:40:14 +0600

    Being up

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_Z30-user
-add_lunch_combo omni_Z30-userdebug
-add_lunch_combo omni_Z30-eng
+add_lunch_combo twrp_Z30-user
+add_lunch_combo twrp_Z30-userdebug
+add_lunch_combo twrp_Z30-eng
